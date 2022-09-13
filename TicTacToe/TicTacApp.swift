117274f2e92d4564c899aca68af96ea2a6010306
Raul_Gasanov <raulfutbollist@gmail.com>
2022-09-13 03:03:40 +0300

    TicTacToe

---
+++ TicTacToe/TicTacApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacApp.swift
+//  TicTac
+//
+//  Created by Raul_Gasanov on 19.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct TicTacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
